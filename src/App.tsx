cbf57be8c211447413a4f7734f6aa9c96243ec4d
Andrelino Silva <andrelinocom@gmail.com>
2022-02-20 19:36:00 -0300

    Sem react query

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,24 @@ type RepositoryProps = {
 };
 
 export default function App() {
-    const { data: repositories } = useFetch<RepositoryProps[]>(
-        'https://api.github.com/users/andrelinos/repos'
+    const { data: repositories, isFetching } = useFetch<RepositoryProps[]>(
+        'users/andrelinos/repos'
     );
 
     return (
         <ul>
-            {repositories?.map((repo) => {
-                return (
-                    <li key={repo.full_name}>
-                        <strong>{repo.full_name}</strong>
-                        <p>{repo.description}</p>
-                    </li>
-                );
-            })}
+            {isFetching ? (
+                <p>Carregando...</p>
+            ) : (
+                repositories?.map((repo) => {
+                    return (
+                        <li key={repo.full_name}>
+                            <strong>{repo.full_name}</strong>
+                            <p>{repo.description}</p>
+                        </li>
+                    );
+                })
+            )}
         </ul>
     );
 }
